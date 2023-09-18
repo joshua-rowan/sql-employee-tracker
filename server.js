d8bf9608cc5060cecc477882cfc51595468be5aa
De La Josh <joshr1979@gmail.com>
2023-09-17 22:17:31 -0500

    initial commit of inquirer calling getDepartments

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 const { Department, Role, Employee } = require('./models');
+const { startScript } = require('./utils/promptScript'); // Import the startScript function
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,8 +10,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 sequelize.sync().then(() => {
-    app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
-})
-.catch((err) => {
+    app.listen(PORT, () => {
+        console.log(`Now listening on ${PORT}`);
+        // Call the startScript function to initiate the Inquirer prompt
+        startScript();
+    });
+}).catch((err) => {
     console.error('Error syncing database:', err);
 });
