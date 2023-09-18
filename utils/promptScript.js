842e20076eb306294a976775251aa473866e5279
De La Josh <joshr1979@gmail.com>
2023-09-18 13:45:47 -0500

    deleted exit code for malfunctioning promptScritp

--- utils/promptScript.js
+++ utils/promptScript.js
@@ -17,26 +17,24 @@ const {
 // ];
 
 async function startScript() {
-    const { action } = await inquirer.prompt([{
-        type: 'list',
-        name: 'prompt',
-        message: 'What would you like to do?',
-        choices: ['View All Departments', 'Exit']
-    }]).then((answers) => {
-        console.log('answers =', answers)
-        if (answers.prompt === 'View All Departments') {
-            getAllDepartments((err, result) => {
-                if(err) throw err;
-                console.log('Viewing Departments: ');
-                console.table(result);
-                startScript();
-            });
-        } else if (answers.prompt === 'Exit') {
-            console.log('Goodbye!');
-            process.exit(0);
-        }
-    })
+    try {
+        const { action } = await inquirer.createPromptModule([{
+            type: 'list',
+            name: 'prompt',
+            message: 'What would you like to do?',
+            choices: ['View All Departments', 'Exit'],
+        }]);
 
+        if (action === 'View All Departments') {
+            const departments = await getAllDepartments();
+            console.table(departments);
+        } else if (action === 'Exit') {
+            console.log('Goodbye!');
+        }
+        startScript();
+    } catch (error) {
+        console.error('An error occurred: ', error.message);
+    }
 };
 
 startScript();
