472a80bc0c71d020e2812c7cd53a25f18bf7132c
De La Josh <joshr1979@gmail.com>
2023-09-19 11:54:43 -0500

    initial commit of working inquirer script

--- index.js
+++ index.js
@@ -4,4 +4,25 @@ const db = require('./db/connection');
 db.connect(err => {
     if(err) throw err;
     console.log('Connected to database.');
-})
+    startScript();
+});
+
+var startScript = function () {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'prompt',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Departments', 
+            'View All Roles', 
+            'View All Employees', 
+            'Add a Department',
+            'Add a Role',
+            'Add an Employee',
+            'Update An Employee Role',
+            'Exit']
+    }]).then((answers) => {
+        console.log('answers: ', answers);
+        startScript();
+    }) 
+};
